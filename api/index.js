dc7d61ba6b1a40736db1bd3777d4a96ba1dbac27
rojer <rojerchen@qq.com>
2022-03-13 16:29:16 +0800

    feat: :boom: init

---
+++ api/index.js
@@ -0,0 +1,7 @@
+import katex from "katex";
+import parse, { loadKatex } from "@rojer/katex-mini";
+loadKatex(katex);
+module.exports = (req, res) => {
+  const { latex = "" } = req.query;
+  res.json(parse(latex));
+};
