92b6b4a006cc8f7a519b5e6d332d0b117c6a470d
rojer <rojerchen@qq.com>
2022-03-16 11:50:40 +0800

    feat: :boom: displayMode

--- api/index.js
+++ api/index.js
@@ -3,5 +3,9 @@ import parse, { loadKatex } from "@rojer/katex-mini";
 loadKatex(katex);
 module.exports = (req, res) => {
   const { latex = "" } = req.query;
-  res.json(parse(latex));
+  res.json(
+    parse(latex, {
+      displayMode: true,
+    })
+  );
 };
